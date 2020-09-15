11c16c0cfe1b24cd6c09568c0a6e2e2dbd1a8413
Donghyun (Mark) Kim <markk628@gmail.com>
2020-09-14 22:15:42 -0700

    implemented persistent container

--- LoanerApp/Loaner/AppDelegate.swift
+++ LoanerApp/Loaner/AppDelegate.swift
@@ -13,9 +13,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let rootViewController = window!.rootViewController as! UINavigationController
+        let viewController = rootViewController.topViewController as! ViewController
+        viewController.store = ItemStore()
         return true
     }
 
---
+++ LoanerApp/Loaner/Model/ItemStore.swift
@@ -0,0 +1,27 @@
+//
+//  ItemStore.swift
+//  loaner
+//
+//  Created by Mark Kim on 9/14/20.
+//  Copyright © 2020 LinnierGames. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ItemStore: NSObject {
+    
+    let persistentContainer: NSPersistentContainer = {
+        // Creates the NSPersistentContainer object
+        // Must be given the name of the Core Data model file "LoanedItems"
+        let container = NSPersistentContainer(name: "LoanedItems")
+        
+        // Load the saved database if it exists, creates if it does not, and reeturns an error if fail
+        container.loadPersistentStores { (description, error) in
+            if let error = error {
+                print("Error setting up Core Data (\(error))")
+            }
+        }
+        return container
+    }()
+}
