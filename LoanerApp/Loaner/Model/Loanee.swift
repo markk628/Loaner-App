2860b68d047fd8a149ee669684863406337ec1b3
Donghyun (Mark) Kim <markk628@gmail.com>
2020-09-14 20:21:08 -0700

    Created entity and attributes

--- LoanerApp/Loaner/Model/Loanee.swift
+++ LoanerApp/Loaner/Model/Loanee.swift
@@ -8,13 +8,39 @@
 
 import UIKit
 
-class Loanee: NSObject {
+class Loanee: NSObject, NSCoding {
 
-        var name: String
-        var contactNumber: String?
+    var name: String
+    var contactNumber: String?
+
+    // NSCoding protocol requires keys for variables
+    enum Keys: String {
+        case name = "name"
+        case contactNumber = "contactNumber"
+    }
+    
+    init(name: String, contactNumber: String?) {
         
-        init(name: String, contactNumber: String?) {
-            self.name = name
-            self.contactNumber = contactNumber
-        }
+        /** For Future Feature: Ability to access Contacts app:
+        init(name: String, profileImage: UIImage, contactNumber: String?) {
+
+        self.profileImage = profileImage
+        **/
+        
+        self.name = name
+        self.contactNumber = contactNumber
+    }
+    
+    // NSCoding requires encoding func
+    func encode(with coder: NSCoder) {
+        coder.encode(name, forKey: "name")
+        coder.encode(contactNumber, forKey: "contactNumber")
+    }
+    
+    // NSCoding requires decoing func
+    required init?(coder aDecoder: NSCoder) {
+        name = aDecoder.decodeObject(forKey: "name") as! String
+        contactNumber = aDecoder.decodeObject(forKey: "contactNumber") as? String
+        super.init()
+    }
 }
